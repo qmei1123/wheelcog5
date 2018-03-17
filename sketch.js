573158dc518ce787989fbc12756d05fa51a3f660
qmei1123 <qmei@bu.edu>
2018-03-17 18:43:51 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,46 @@
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    angleMode(DEGREES);
+    frameRate(.75);
+}
+
+function draw() { 
+    background(209,209,246);
+    //line following mouse
+    strokeWeight(random(1,2));
+
+     
+    for (i = 30; i < width - 140; i+=65) {
+        for (j = height/2; j < height; j+=20) {
+  //custom shape 
+            beginShape();
+            noFill();
+            stroke(255,255,237);
+
+            let vertices = map(mouseX, 0, width, 5, 50);
+
+            for(let a = 0; a < 360; a += vertices) {
+                let x = 25 * sin(a) + 180;
+                let y = 50 * cos(a) + 180;
+                vertex (x + random(i-10, i) - 90, y + random(j));
+            }
+            endShape(CLOSE);
+        }
+    }
+    
+    for (i = 0; i < width - 140; i+=65) {
+        for (j = height/2; j > 0; j-=70) {
+            beginShape();
+            noFill;
+            stroke(47,103,221);
+            let verticesTwo = map(mouseY, 0, height, 3, 50);
+
+            for(let b = 0; b < 360; b += verticesTwo) {
+                let x2 = 30 * cos(b) + 180;
+                let y2 = 50 * sin(b) + 200;
+                vertex (x2 + random(i-10,i) - 90, y2 + random(-j, j) - 90);
+            }
+            endShape(CLOSE);
+        }
+    }
+}
