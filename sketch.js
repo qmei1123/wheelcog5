ae7d682d3bd5f1c3130a978377bf4ecfaef43ae6
qmei1123 <qmei@bu.edu>
2018-03-18 09:18:51 -0400

    adjust colors and add new layer

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ function setup() {
 }
 
 function draw() { 
-    background(209,209,246);
+    background(47,103,221);
     //line following mouse
-    strokeWeight(random(1,2));
+    strokeWeight(1.2);
 
      
     for (i = 30; i < width - 140; i+=65) {
@@ -23,6 +23,24 @@ function draw() {
                 let x = 25 * sin(a) + 180;
                 let y = 50 * cos(a) + 180;
                 vertex (x + random(i-10, i) - 90, y + random(j));
+            }
+            endShape(CLOSE);
+        }
+    }
+    
+    for (i = -180; i < width; i+=65) {
+        for (j = height/8; j < height/2; j+=20) {
+  //custom shape 
+            beginShape();
+            noFill();
+            stroke(233,150,122);
+
+            let vertices = map(mouseX, 0, width, 5, 50);
+
+            for(let a = 0; a < 360; a += vertices) {
+                let x = 25 * sin(a) + 180;
+                let y = 50 * cos(a) + 180;
+                vertex (x + random(i-10, i+10) - 90, y + random(j-10, j+10));
             }
             endShape(CLOSE);
         }
